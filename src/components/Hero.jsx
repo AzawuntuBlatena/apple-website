26fb5323646802ffe400dcfadc57fb54a8496c86
AzawuntuBlatena <azabla1227@gmail.com>
2024-08-03 23:04:06 +0300

    the 3d model is added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 const Hero = () => {
     useGSAP(()=>{
-        gsap.to("#hero", {opacity: 1, delay: 1.5})
+        gsap.to("#hero", {opacity: 1, delay: 1.5}),
+        gsap.to("#cta", {opacity: 1, y:-50, delay:2})
     },[])
 
     const[videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
@@ -28,15 +29,20 @@ const Hero = () => {
 
   return (
     <section className='w-full nav-height bg-black relative'>
-        <div className='w-full h-5/6 flex-center flex-col '>
+        <div className='w-full h-[80%] flex-center flex-col '>
             <p id='hero' className='hero-title'>iPhone 15 Pro</p>
 
             <div className='md:w-10/12 w-9/12'>
                 <video className='pointer-events-none'
-                autoPlay muted playsInline="true" key={videoSrc}>
+                autoPlay muted playsInline={true} key={videoSrc}>
                     <source src={videoSrc}/>
                 </video>
             </div>
+        </div>
+
+        <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+            <a href='#highlights' className='btn'>buy</a>
+            <p className='font-normal text-xl'>From $199/month or $999</p>
         </div>
     </section>
   )
